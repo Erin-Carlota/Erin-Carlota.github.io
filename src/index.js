63e334c2e9c302a5a96050592d711f3f3565046d
Erin-Carlota <calmg369@gmail.com>
2020-10-14 16:07:43 +0800

    文档优化

---
+++ src/index.js
@@ -0,0 +1,15 @@
+export {default as Button} from './button/button'
+export {default as ButtonGroup} from './button/button-group'
+export {default as Icon} from './icon/icon'
+export {default as Input} from './input/input'
+export {default as Menu} from './menu/menu'
+export {default as MenuItem} from './menu/menu-item'
+export {default as SubMenu} from './menu/sub-menu'
+
+export {default as Spin} from './spin/spin'
+export {default as Switch} from './switch/switch'
+export {default as Tabs} from './tabs/tabs'
+export {default as TabsPane} from './tabs/tabs-pane'
+export {default as TabsTitle} from './tabs/tabs-title'
+
+
